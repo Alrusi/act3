c6d47045245f02248f464a5f8f5f626c05a4522b
Alrusi <al07141774@tecmilenio.mx>
2025-09-05 14:03:15 -0600

    Add files via upload

---
+++ act3.py
@@ -0,0 +1,13 @@
+print("==============================================================================")
+print("\tBienvenido a La TecmiCalculadora de consumo energetico y costos")
+print("==============================================================================") 
+
+# Guardado de datos
+def mostrar_menu():
+    print("1. Mostrar la tabla completa con su consumo")
+    print("2. Consultar el gasto energético y costo mensual de un dispositivo en particular")
+    print("3. Consultar el gasto energético y costo mensual de un dispositivo en particular")
+    print("4. Salir")
+
+def main():
+    dispositivos = []
